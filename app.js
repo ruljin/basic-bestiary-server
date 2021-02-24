acf91b8512224bd34ce7efa0d3eb1aa790a050e5
fkuca <fkuca91@gmail.com>
2021-02-24 20:01:36 +0100

    Add nodemon dev dependency and on error server listener

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const http = require("http");
 const url = require("url");
 const monsters = require("./controllers/monsters");
-const errors = require("./errors");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const requestHaveQueryParam = url.parse(req.url).query;
-  if (req.url.includes("/monsters")) {
-    if (!requestHaveQueryParam && req.url === "/monsters/all") {
-      monsters.getAllMonsters();
-    } else if (requestHaveQueryParam) {
-      monsters.getMonster(requestHaveQueryParam.split("=")[1]);
+const server = http
+  .createServer((req, res) => {
+    const requestHaveQueryParam = url.parse(req.url).query;
+    if (req.url.includes("/monsters")) {
+      if (!requestHaveQueryParam && req.url === "/monsters/all") {
+        monsters.getAllMonsters();
+      } else if (requestHaveQueryParam) {
+        monsters.getMonster(requestHaveQueryParam.split("=")[1]);
+      }
     }
-  } else {
-    throw Error(`${errors.WRONG_PATH}`);
-  }
-});
+  })
+  .on("error", (error) => {
+    throw error;
+  });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  // console.log(server.address());
+  // to get address, IP family and port
 });
+
+// https://medium.com/@grantspilsbury/dry-node-js-server-code-without-express-4db391a9ac60
+// https://www.youtube.com/watch?v=H9fg7GFagF4&list=PLgTkNlNsy9gVPkxTJry5zb5V8UwLZhw3q&index=1
